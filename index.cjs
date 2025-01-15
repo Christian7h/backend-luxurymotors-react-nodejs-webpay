bf49dc257dca1e1d20c91f340c0b8ef8e0856cb4
Cristian Villalobos <cristianvillalobos666@gmail.com>
2025-01-15 18:19:18 -0300

    corrige un error tipográfico en el mensaje de inicio del servidor

--- index.cjs
+++ index.cjs
@@ -125,5 +125,5 @@ app.post("/api/send-email", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on portT ${PORT}`);
 });
